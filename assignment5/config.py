aabfd982dee5bdd4c49068d782adaf44e1a3c5c0
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-05-12 14:02:19 +0900

    done with semester

--- assignment5/config.py
+++ assignment5/config.py
@@ -3,13 +3,13 @@ EPISODES = 3500
 HEIGHT = 84
 WIDTH = 84
 HISTORY_SIZE = 4
-learning_rate = 0.001
+learning_rate = 0.005
 evaluation_reward_length = 100
 Memory_capacity = 1000000
 train_frame = 100000
 batch_size = 32
-scheduler_gamma = 0.4
-scheduler_step_size = 100000
+scheduler_gamma = 0.8
+scheduler_step_size = 90000
 
 # Hyperparameters for Double DQN agent
 update_target_network_frequency = 1000
